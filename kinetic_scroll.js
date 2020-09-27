18570a8c23f9b03973475ecf05806db5e36fa7a3
kjanczak <kubjan4@gmail.com>
2020-09-27 12:02:34 +0200

    Scrolling down works!

--- kinetic_scroll.js
+++ kinetic_scroll.js
@@ -1,12 +1,55 @@
-let lastDeltaY = [0, 0, 0]
+let lastDeltaY = [];
+for (let i = 0; i < 50; i++) lastDeltaY.push({distance: 0,time: 0});
+
 let lastScrollEventTime = 0;
 
-window.onwheel = function (ev) {
-    lastDeltaY[0] = lastDeltaY[1];
-    lastDeltaY[1] = lastDeltaY[2];
-    lastDeltaY[2] = ev.deltaY;
+let kineticScrollEnd = false;
+let scrollLoop = false;
 
-    //console.log(Date.now() - lastScrollEventTime + " " + lastDeltaY[0]);
-    console.log(lastDeltaY)
-    lastScrollEventTime = Date.now();
+setInterval(() => {
+    if (!scrollLoop && !kineticScrollEnd) {
+
+        let averageScroll = 0;
+        let addedScrolls = 0
+        let timeNow = Date.now();
+        lastDeltaY.forEach(deltaY => {
+            if (timeNow - deltaY.time < 100) {
+                averageScroll += deltaY.distance;
+                addedScrolls++;
+            }
+        });
+        averageScroll /= addedScrolls;
+
+        console.log(addedScrolls);
+
+        scrollLoop = setInterval(() => {
+            window.scrollTo(0, window.scrollY + averageScroll);
+            averageScroll -= 3;
+
+            if (averageScroll < 0) {
+                kineticScrollEnd = true;
+                clearInterval(scrollLoop);
+                scrollLoop = false;
+            }
+        }, 16);
+    }
+}, 50);
+
+window.onwheel = function (ev) {
+    if (scrollLoop) {
+        clearInterval(scrollLoop);
+        scrollLoop = false;
+    }
+    kineticScrollEnd = false;
+
+    for (let i = 0; i < lastDeltaY.length; i++) {
+        if (i != lastDeltaY.length - 1) {
+            lastDeltaY[i].distance = lastDeltaY[i + 1].distance
+            lastDeltaY[i].time = lastDeltaY[i + 1].time
+        }
+        else {
+            lastDeltaY[i].distance = ev.deltaY;
+            lastDeltaY[i].time = Date.now();
+        }
+    }
 }
